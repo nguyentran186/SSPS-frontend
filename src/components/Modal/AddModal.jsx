6a8556c11ca1bd024df0fc170df4d07a956905b9
nminh <minhlui254qn254@gmail.com>
2023-11-10 20:34:48 +0700

    aa

--- src/components/Modal/AddModal.jsx
+++ src/components/Modal/AddModal.jsx
@@ -73,14 +73,14 @@ export default function AddModal(...props) {
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
-                                        Close
+                                        Đóng
                                     </button>
                                     <button
                                         className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
-                                        Save Changes
+                                        Thêm tài khoản
                                     </button>
                                 </div>
                             </div>
