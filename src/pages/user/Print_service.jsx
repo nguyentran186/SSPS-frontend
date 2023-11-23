e95ead684bce25766eb0b421e9724352993996b9
Tran Minh Triet <tranminhtriet0@gmail.com>
2023-11-23 23:57:56 +0700

    ignore this commit

--- src/pages/user/Print_service.jsx
+++ src/pages/user/Print_service.jsx
@@ -64,38 +64,36 @@ function Print_service({ ...props }) {
         <main className="h-full"> 
           <div className="px-2 mx-auto mainCard h-full overflow-hidden">
             <div className="w-full text-slate-700 md:grid gap-4 grid md:grid-cols-6 h-full overflow-hidden">
-            <div className="h-full rounded-lg md:col-start-1 md:col-span-4 md:row-span-2 flex-col items-center justify-center overflow-hidden bg-green-200">
-           
-            
-            <div className="h-full rounded-lg md:col-start-1 md:col-span-4 md:row-span-2 flex-col items-center justify-center overflow-hidden bg-green-200 p-6">
-              <div className="grid grid-cols-4 gap-5 overflow-y-auto max-h-full items-start">
-              {files.map((file, index) => (
-                <div key={index} className="flex flex-col items-center rounded p-4 bg-[#D9F99D] w-full h-48 justify-center">
-                  {(file.type === 'application/pdf' || file.type === 'application/msword' || file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') ? (
-                    <img src={getRandomImage()} alt="Placeholder for document" className="w-5/6 h-5/6 object-cover mb-2" />
-                  ) : (
-                    <img src={URL.createObjectURL(file)} alt={file.name} className="w-5/6 h-5/6 object-cover mb-2" />
-                  )}
-                  <p className="text-s">{file.name.length > 15 ? file.name.substring(0, 15) + '...' : file.name}</p>
-              
+              <div className="h-full rounded-lg md:col-start-1 md:col-span-4 md:row-span-2 flex-col items-center justify-center overflow-hidden bg-green-200">
+                <div className="h-full rounded-lg md:col-start-1 md:col-span-4 md:row-span-2 flex-col items-center justify-center overflow-hidden bg-green-200 p-6">
+                  <div className="grid grid-cols-4 gap-5 overflow-y-auto max-h-full items-start">
+                    {files.map((file, index) => (
+                      <div key={index} className="flex flex-col items-center rounded p-4 bg-[#D9F99D] w-full h-48 justify-center">
+                        {(file.type === 'application/pdf' || file.type === 'application/msword' || file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') ? (
+                          <img src={getRandomImage()} alt="Placeholder for document" className="w-5/6 h-5/6 object-cover mb-2" />
+                        ) : (
+                          <img src={URL.createObjectURL(file)} alt={file.name} className="w-5/6 h-5/6 object-cover mb-2" />
+                        )}
+                        <p className="text-s">{file.name.length > 15 ? file.name.substring(0, 15) + '...' : file.name}</p>
+                    
+                      </div>
+                    ))}
+
+                  </div>
                 </div>
-              ))}
 
               </div>
-            </div>
-
-            </div>
-  
-              <div className="md:col-start-5 md:col-span-2 h-full overflow-hidden">
-                <AchievementWidget />
+    
+                <div className="md:col-start-5 md:col-span-2 h-full overflow-hidden">
+                  <AchievementWidget />
+                </div>
+                <button onClick={handleButtonClick} className="md:col-start-3 md:col-span-1 md:row-start-3 bg-emerald-400 hover:bg-emerald-600 text-white font-bold rounded h-12">
+                  Thêm tệp
+                </button>
+                <button /*onClick= to next page */ className="md:col-start-4 md:col-span-1 md:row-start-3 bg-emerald-400 hover:bg-emerald-600 text-white font-bold rounded h-12">
+                  Tiếp tục
+                </button> 
               </div>
-              <button onClick={handleButtonClick} className="md:col-start-3 md:col-span-1 md:row-start-3 bg-emerald-400 hover:bg-emerald-600 text-white font-bold rounded h-12">
-                Thêm tệp
-              </button>
-              <button /*onClick= to next page */ className="md:col-start-4 md:col-span-1 md:row-start-3 bg-emerald-400 hover:bg-emerald-600 text-white font-bold rounded h-12">
-                Tiếp tục
-              </button> 
-            </div>
             
           </div>
         </main>
