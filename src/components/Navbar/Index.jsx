d2bddf21c1e32ee8cee77e25976191920b81ea5d
Tran Minh Triet <tranminhtriet0@gmail.com>
2023-12-05 01:16:16 +0700

    added avatar

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -2,6 +2,8 @@ import { faBars, faBell, faMessage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext } from "react";
 
+import ava from "../../assets/images/ava1.jpg"
+
 function Index({ toggle }) {
   const avatar =
     "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
@@ -32,7 +34,7 @@ function Index({ toggle }) {
                   <span>
                     <img
                       className="rounded-full h-9 w-9 border cursor-pointer"
-                      src={avatar}
+                      src={ava}
                       alt="Avatar"
                     />
                   </span>
--- src/components/Other/DashboardHeader.jsx
+++ src/components/Other/DashboardHeader.jsx
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import ava from "../../assets/images/ava1.jpg"
+
 function DashboardHeader({ user, avatar, toggle }) {
   return (
     <div className="px-3 sm:px-8 pt-9 pb-4 flex flex-wrap w-full justify-between items-center">
@@ -15,7 +17,7 @@ function DashboardHeader({ user, avatar, toggle }) {
         <p className="flex-shrink-0 rounded-full block md:hidden border border-emerald-400 p-[3px] shadow-lg">
           <img
             className="rounded-full md:h-14 md:w-14 h-10 w-10 border cursor-pointer"
-            src={avatar}
+            src={ava}
             alt="Avatar"
           />
         </p>
@@ -41,7 +43,7 @@ function DashboardHeader({ user, avatar, toggle }) {
         <p className="rounded-full hidden md:block border border-emerald-400 p-[3px] shadow-lg">
           <img
             className="rounded-full md:h-14 md:w-14 h-10 w-10 border cursor-pointer"
-            src={avatar}
+            src={ava}
             alt="Avatar"
           />
         </p>
